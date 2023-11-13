35d245e852fe936edef8555d89fa182eb1c43a4c
Justin <aabdubannopov6@gmail.com>
2023-11-13 21:59:12 +0300

    .

--- components/headerBase.vue
+++ components/headerBase.vue
@@ -1,20 +1,37 @@
 <template>
     <div>
         <div class="header">
-            <div class="header-inner">
+            <div class="header-inner container mx-auto flex items-center justify-between">
                 <a href="#" class="logo">
                     <img src="~/assets/images/svg/logo.svg" alt="logo" width="216" height="68">
                 </a>
-                <div class="header-menu">
-                    <nav>
-                        <ul>
-                            <li><a href="#">Bosh sahifa</a></li>
-                            <li>Biz haqimizda</li>
-                            <li><a href="#">Tadbirlar</a></li>
-                            <li><a href="#">Yangiliklar</a></li>
-                            <li>Volontyor</li>
+                <div class="header-menu flex items-center">
+                    <nav class="mr-4">
+                        <ul class="flex items-center gap-x-8">
+                            <li><a class="text-base" href="#">Bosh sahifa</a></li>
+                            <li><div class="flex items-center gap-x-2"><span class="text-base">Biz haqimizda</span><img src="~/assets/images/svg/chevron.svg" alt="chevron"></div></li>
+                            <li><a class="text-base" href="#">Tadbirlar</a></li>
+                            <li><a class="text-base" href="#">Yangiliklar</a></li>
+                            <li><div class="flex items-center gap-x-2">
+                                <span class="text-base">Volontyor</span><img src="~/assets/images/svg/chevron.svg" alt="chevron">
+                            </div></li>
                         </ul>
                     </nav>
+                    <div class="header-menu-bar flex items-cente">
+                        <div class="search mr-4">
+                            <button class="search-btn p-2">
+                                <img src="~/assets/images/svg/search.svg" alt="search" width="24" height="24">
+                            </button>
+
+                        </div>
+                        <div class="lang mr-2">
+                            <button class="lang-btn p-2 rounded-md">
+                                <img src="~/assets/images/svg/globe.svg" alt="" class="lang-btn-img">
+                            </button>
+                        </div>
+                        <a href="#" class="header-menu-bar-links join p-2 mr-2">Azo bo'lish</a>
+                        <a href="#" class="header-menu-bar-links donation p-2 ">Hayriya</a>
+                    </div>
                 </div>
             </div> 
         </div>
