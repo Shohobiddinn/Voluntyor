5305a007fe8b6fa0e7fc07fc0f5fb6573d552efe
Justin <aabdubannopov6@gmail.com>
2023-11-14 21:14:14 +0300

    cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,46 @@
 <template>
     <div>
         <headerBase />
+
+        <!-- cards  -->
+            <div class="cards container mx-auto">
+                <div class="card flex items-center justify-between">
+                    <div class="card-item">
+                        <h2 class="card-item-title text-2xl font-semibold mb-8">Kuzatuvchular</h2>
+                        <img src="../assets/images/png/vision.png" alt="vision" class="card-item-img mb-10" width="100" height="100">
+                        <p class="card-item-text">2,345</p>
+                    </div>
+                    <div class="card-item">
+                        <h2 class="card-item-title text-2xl font-semibold mb-8">Volunterlar</h2>
+                        <img src="../assets/images/png/Ixtiyoriylik.png" alt="ixtiyoriylik" class="card-item-img mb-10"  width="100" height="100">
+                        <p class="card-item-text">2,345</p>
+                    </div>
+                    <div class="card-item">
+                        <h2 class="card-item-title text-2xl font-semibold mb-8">Investor</h2>
+                        <img src="../assets/images/png/partners.png" alt="partners" class="card-item-img mb-10"  width="100" height="100">
+                        <p class="card-item-text">2,345</p>
+                    </div>
+                    <div class="card-item">
+                        <h2 class="card-item-title text-2xl font-semibold mb-8">Tadbirlar</h2>
+                        <img src="../assets/images/png/Projects.png" alt="projects" class="card-item-img mb-10"  width="100" height="100">
+                        <p class="card-item-text">2,345</p>
+                    </div>
+                </div>
+            </div>
+        <!-- cards end -->
+
+        <!-- volontyor -->
+        <div class="volontyor container mx-auto flex items-center gap-x-8">
+            <img src="~/assets/images/jpg/volontyor-bg.jpg" alt="bg" class="volontyor-img" width="568" height="230">
+            <div class="volontyor-wrapper">
+                <h2 class="mb-4">Volontyorlar kerekmi ?</h2>
+                <p class="volontyor-text volontyor-text-one text-base font-semibold">Agarda sizga volontyor kerak bolsa, bizning volontyorlar siz uchun doimo <br> yordamga tayyor</p>
+                <div class="volontyor-divider"></div>
+                <p class="volontyor-text volontyor-text-two text-base font-semibold mb-4">Volontyorlar hizmatidan foydalanish uchun bosing</p>
+                <a href="#" class="text-2xl py-3">Volontyorlar</a>
+            </div>
+        </div>
+        <!-- volontyor end -->
     </div>
 </template>
 <script setup>
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+module.exports= {
+    theme: {
+        container: {
+            screens: {
+                'sm': '100%',
+                'md': '100%',
+                'lg': '1024px',
+                'xl': '1200px',
+                '2xl': '1600px',
+            }
+        }
+    }
+}
