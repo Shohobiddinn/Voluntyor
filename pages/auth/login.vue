9bc6e8890592b28ed140c037ce6747359d232c29
Justin <aabdubannopov6@gmail.com>
2023-11-18 19:45:13 +0300

    18.11

---
+++ pages/auth/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <img id="imga" src="~/assets/images/svg/auth-bg.svg" alt="">  
+    </div>
+</template>
+<script setup>
+</script>
+<style>
+#imga{
+    width: 150vw;
+    height: 1027px;
+}
+
+</style>
